38169c2e7688e6bd568dbfaf4ef9d97fe999f556
benel-del <52874329+benel-del@users.noreply.github.com>
2020-10-13 02:04:47 +0900

    [pa2]

--- src/pa2/closest.java
+++ src/pa2/closest.java
@@ -1,5 +1,38 @@
-package pa2;
+import java.io.*;
 
-public class closest {
+// DO NOT modify class point, function closest(), and function setPoints(point p[])!
 
+class point{
+	public double x;
+	public double y;
+	public point(){
+		x = 0.0;
+		y = 0.0;
+	}
+}
+
+public class closest{
+	private point pnt[];
+
+// If you need more member variables or member functions, declare here...
+        
+	public closest(){
+		this.pnt = null;
+	}
+
+	public void setPoints(point p[]){
+		int i = 0, n = p.length;
+		this.pnt = null;
+		this.pnt = new point[n];
+		for(i=0;i<n;i++){
+			this.pnt[i] = new point();
+			this.pnt[i].x = p[i].x;
+			this.pnt[i].y = p[i].y;
+		}
+	}
+	public double getMinDist(){
+		//Input source code here...
+		
+		return -1.0;
+    }
 }
