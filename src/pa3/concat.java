dd0dd02df20fd9b39ed17ca571669968fd2004d5
benel-del <52874329+benel-del@users.noreply.github.com>
2020-11-06 20:40:32 +0900

    [pa3] concat.java (코드 정리)
    Q. StringBuffer를 써도 되는 것인가..

--- src/pa3/concat.java
+++ src/pa3/concat.java
@@ -3,67 +3,70 @@ package pa3;
 import java.io.*;
 
 public class concat{
-        private pri[] pa;
+    private pri[] pa;	// pa[] { int: pr, string: word }
 
 // Define additional variables or functions if you need...
-        private int n, k, max;
-        private int i;
+    private int i;	// for FOR statement
+    private int n;	// array length
+    
+    /* counting sort */
+    private String[] pb;	// the result of sorting - standard value: pr, result value: word
+    private int max;	// for size of pc[]
+
+    public concat(){
+        this.pa = null;
+    }
+
+    public concat(int[] priority, String[] words){
+        int i = 0, n = priority.length;
+        this.pa = new pri[n];
+        for(i=0;i<n;i++)        this.pa[i] = new pri(priority[i], words[i]);
+        return;
+    }
+
+    public String concatenate(){
+        String result = "";
+
+        // Insert source code here...
+        n = pa.length;
+
+        getMax();	// for max
+        countingSort();	// for STABLE sorting
         
-        /* counting sort */
-        private String[] pb;
-
-        public concat(){
-                this.pa = null;
+        StringBuffer buffer = new StringBuffer();	// for 'string + string'
+        for(i = 0; i < n; i++) {
+        	//result += pb[i] + " ";	// score: 1.4s ~ 1.6s
+        	buffer.append(pb[i] + " ");	// score: 14ms ~ 0.16ms
         }
-
-        public concat(int[] priority, String[] words){
-                int i = 0, n = priority.length;
-                this.pa = new pri[n];
-                for(i=0;i<n;i++)        this.pa[i] = new pri(priority[i], words[i]);
-                return;
-        }
-
-        public String concatenate(){
-                String result = "";
-
-                // Insert source code here...
-                n = pa.length;
-                k = n/2;
-
-                getMax();
-                countingSort();
-                
-                for(i = 0; i < n; i++) {
-                	result += pb[i] + " ";
-                }
-
-                return result;
-        }
-        
-        private void getMax() {
-        	max = pa[0].pr;
-        	for(i = 1; i < n; i++) {
-        		if(pa[i].pr > max)
-        			max = pa[i].pr;
-        	}
-        }
-        
-        private void countingSort(){
-        	int[] pc = new int[max+1];
-        	pb = new String[n];
-        	for(i = 0; i <= max; i++)
-        		pc[i] = 0;
-        	for(i = 0; i < n; i++)
-        		pc[pa[i].pr]++;
-        	for(i = 1; i <= max; i++)
-        		pc[i] += pc[i-1];
-        	for(i = n - 1; i >= k; i--) {
-        		pc[pa[i].pr]--;
-        		pb[pc[pa[i].pr]] = new String(pa[i].word);
-        	}
-        	for(; i >= 0; i--) {
-        		pc[pa[i].pr]--;
-        		pb[pc[pa[i].pr]] = new String("-");
-        	}
-        }
+        result = buffer.toString();
+        return result;
+    }
+    
+    private void getMax() {
+    	max = pa[0].pr;
+    	for(i = 1; i < n; i++) {
+    		if(pa[i].pr > max)
+    			max = pa[i].pr;
+    	}
+    }
+    
+    private void countingSort(){
+    	int[] pc = new int[max + 1];	// assistant array
+    	pb = new String[n];
+    	
+    	for(i = 0; i <= max; i++)	// initialization pc
+    		pc[i] = 0;
+    	for(i = 0; i < n; i++)
+    		pc[pa[i].pr]++;
+    	for(i = 1; i <= max; i++)	// Cumulative
+    		pc[i] += pc[i-1];
+    	for(i = n - 1; i >= n / 2; i--) {	// i >= floor(n/2)
+    		pc[pa[i].pr]--;
+    		pb[pc[pa[i].pr]] = new String(pa[i].word);
+    	}
+    	for(; i >= 0; i--) {	// i < floor(n/2) -> "-"
+    		pc[pa[i].pr]--;
+    		pb[pc[pa[i].pr]] = new String("-");
+    	}
+    }
 }
