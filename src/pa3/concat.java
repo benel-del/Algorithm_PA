781b6ae5d6b13713e948736b329d541f9e975dfe
benel-del <52874329+benel-del@users.noreply.github.com>
2020-11-08 05:58:09 +0900

    [pa3] complete concat.java
    op time: 2.3~3.2 ms

--- src/pa3/concat.java
+++ src/pa3/concat.java
@@ -6,11 +6,13 @@ public class concat{
     private pri[] pa;	// pa[] { int: pr, string: word }
 
 // Define additional variables or functions if you need...
-    private int i;	// for FOR statement
-    private int n;	// array length
+    private int i, j;	// for FOR statement
+    private int n, m = 0;	// n: array length pa[], m: sum of length pb[i]
+    private char[] rs;	// the set of entire character of pb[] for RESULT
     
     /* counting sort */
     private String[] pb;	// the result of sorting - standard value: pr, result value: word
+    private int[] pc;	// assistant array
     private int max;	// for size of pc[]
 
     public concat(){
@@ -25,48 +27,37 @@ public class concat{
     }
 
     public String concatenate(){
-        String result = "";
-        //char[] arr = new char[];
+        String result;
 
         // Insert source code here...
         n = pa.length;
-
-        getMax();	// for max
+        getValue();	// get max & sum of array length
         countingSort();	// for STABLE sorting
         
-        
-        //StringBuffer buffer = new StringBuffer();	// for 'string + string'
-        result += pb[0];
-        for(i = 1; i < n; i++) {
-        	result += " " + pb[i];	// score: 5.6, time: 1.4s ~ 1.6s
-        	//result = result.concat(pb[i] + " ");	// score: 5.6, time: 1.9 ~ 2.2 
-        	//buffer.append(pb[i] + " ");	// score: 14ms ~ 0.16ms
-        	
+        rs = new char[m + n - 1 + n / 2];	// m을 pa[]에서 구했기 때문에 n/2 필요
+        for(i = max = 0; i < n; i++) {
+        	for(j = 0; j < pb[i].length(); j++)
+        		rs[max++] = pb[i].charAt(j);	// max: not meaning of max. just index
+        	if(i != n - 1)
+        		rs[max++] = ' ';
         }
-        //result = buffer.toString();
         
-        /*
-        int k = 0;
-        for(i = 0; i < n; i++) {
-        	for(int j = 0; j < pb[i].length(); j++) {
-        		arr[k] = pb[i].charAt(j);
-        	}
-        }
-        */
-        
+        result = new String(rs);
         return result;
     }
     
-    private void getMax() {
+    private void getValue() {
     	max = pa[0].pr;
     	for(i = 1; i < n; i++) {
     		if(pa[i].pr > max)
     			max = pa[i].pr;
+    		if(i >= n / 2)	// the condition 4)
+    			m += pa[i].word.length();
     	}
     }
     
     private void countingSort(){
-    	int[] pc = new int[max + 1];	// assistant array
+    	pc = new int[max + 1];
     	pb = new String[n];
     	
     	for(i = 0; i <= max; i++)	// initialization pc
@@ -75,21 +66,11 @@ public class concat{
     		pc[pa[i].pr]++;
     	for(i = 1; i <= max; i++)	// Cumulative
     		pc[i] += pc[i-1];
-    	for(i = n - 1; i >= n / 2; i--) {	// i > floor(n/2)
-    		pc[pa[i].pr]--;
-    		pb[pc[pa[i].pr]] = new String(pa[i].word);
-    	}
-    	for(; i >= 0; i--) {	// i < floor(n/2) -> "-"
-    		pc[pa[i].pr]--;
-    		pb[pc[pa[i].pr]] = new String("-");
-    	}
+    	for(i = n - 1; i >= n / 2; i--)	// i > floor(n/2)
+    		//pc[pa[i].pr]--;
+    		pb[--pc[pa[i].pr]] = new String(pa[i].word);
+    	for(; i >= 0; i--)	// the condition 4) i < floor(n/2) -> "-"
+    		//pc[pa[i].pr]--;
+    		pb[--pc[pa[i].pr]] = new String("-");
     }
 }
-// 남은건 문자열 합치기...
-// 참고 사이트 : https://wikidocs.net/276
-/*
-두 개의 예제 모두 결과는 동일하지만 내부적으로 객체가 생성되고 메모리가 사용되는 과정은 다르다.
-첫번 째 예제의 경우 StringBuffer 객체는 단 한번만 생성된다.
-두번 째 예제는 String 자료형에 + 연산이 있을 때마다 새로운 String 객체가 생성된다(문자열 간 + 연산이 있는 경우 자바는 자동으로 새로운 String 객체를 만들어 낸다).
-두번 째 예제에서는 총 4개의 String 자료형 객체가 만들어지게 된다.
-*/
