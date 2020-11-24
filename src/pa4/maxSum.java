9580db290553a3265f17d74d1694ba26e55633ab
benel-del <52874329+benel-del@users.noreply.github.com>
2020-11-24 21:39:13 +0900

    [pa4] setting

---
+++ src/pa4/maxSum.java
@@ -0,0 +1,25 @@
+package pa4;
+
+public class maxSum{
+    private int []money;
+
+//Declare and define additional variables or functions here if you need...
+
+    public maxSum(int[] m){
+
+            // DO NOT modify constructor
+
+            int i=0, n=m.length;
+            money = new int[n];
+            for(i=0;i<n;i++)        this.money[i] = m[i];
+            return;
+    }
+
+    public int max(){
+            int r = 0;
+
+            // Insert source code here...
+            
+            return r;
+    }
+}
