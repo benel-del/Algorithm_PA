6fd615df59c0cbb469a297af74ec27e2c1b3113a
benel-del <52874329+benel-del@users.noreply.github.com>
2020-11-26 04:08:46 +0900

    [pa4] time over:: 392000us

--- src/pa4/maxSum.java
+++ src/pa4/maxSum.java
@@ -4,6 +4,7 @@ public class maxSum{
     private int []money;
 
 //Declare and define additional variables or functions here if you need...
+    int count = 1;
     public maxSum(int[] m){
 
             // DO NOT modify constructor
@@ -18,11 +19,11 @@ public class maxSum{
             int r = 0;
 
             // Insert source code here...
-            r = sum(money.length-1,1);
+            r = sum(money.length-1);
             return r;
     }
     	
-    private int sum(int n, int count) {
+    private int sum(int n) {
     	int q = 0;
     	if(n == 0) {
     		if(count < 3)
@@ -30,8 +31,11 @@ public class maxSum{
     		else
     			return 0;
     	}
-    	if(count < 3)
-			q = sum(n-1, count+1) + money[n];
-    	return Math.max(q, sum(n-1, 1));
+    	if(count < 3) {
+    		count++;
+    		q = sum(n-1) + money[n];
+    	}
+    	count = 1;
+    	return Math.max(q, sum(n-1));
     }
 }
