a2838f1499c739eee1479bbb1f564eaea439a3fc
benel-del <52874329+benel-del@users.noreply.github.com>
2020-11-29 04:53:50 +0900

    [pa4] Math.max delete

--- src/pa4/maxSum.java
+++ src/pa4/maxSum.java
@@ -17,17 +17,22 @@ public class maxSum{
 
     public int max(){
         int r = 0;
-        r = money.length;
-        value = new int[2][r];
-    	for(int i = 1; i < r; i++) {
+        int p = money.length;
+        
+        value = new int[2][p];
+    	for(int i = 1; i < p; i++) {
     		value[0][i] = value[1][i] = -1;
     	}
     	value[0][0] = 0;
     	value[1][0] = money[0];
 
-    	r = Math.max(sum(r-1, 0), sum(r-1, 1));
+    	r = sum(p-1, 0);
+    	if(r < (p = sum(p-1, 1)))
+    		r = p;
+    	
         return r;
     }
+    
     private int sum(int n, int count) {
     	int p = 0;
     	if(count > 0)
@@ -40,7 +45,9 @@ public class maxSum{
     	if(n > 0) {
     		int q = sum(n-1, 0);
     		if(count < 2) {
-    			q = Math.max(q, sum(n-1, count+1));
+    			int r;
+    			if(q < (r = sum(n-1, count+1)))
+    				q = r;
     		}
     		value[p][n] = q + money[n] * p;
     	}
