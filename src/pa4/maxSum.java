c5061158ef92f24ff09e97f6dcb65f764c6e6e81
benel-del <52874329+benel-del@users.noreply.github.com>
2020-11-29 05:04:50 +0900

    [pa4] value[][] -> value[], value[] :: 250us

--- src/pa4/maxSum.java
+++ src/pa4/maxSum.java
@@ -2,7 +2,7 @@ package pa4;
 
 public class maxSum{
     private int []money;
-    int value[][];
+    int value1[], value2[];
 
 //Declare and define additional variables or functions here if you need...
     public maxSum(int[] m){
@@ -19,12 +19,13 @@ public class maxSum{
         int r = 0;
         int p = money.length;
         
-        value = new int[2][p];
+        value1 = new int[p];
+        value2 = new int[p];
     	for(int i = 1; i < p; i++) {
-    		value[0][i] = value[1][i] = -1;
+    		value1[i] = value2[i] = -1;
     	}
-    	value[0][0] = 0;
-    	value[1][0] = money[0];
+    	value2[0] = 0;
+    	value1[0] = money[0];
 
     	r = sum(p-1, 0);
     	if(r < (p = sum(p-1, 1)))
@@ -36,9 +37,9 @@ public class maxSum{
     private int sum(int n, int count) {
     	int q;
     	if(count > 0)
-    		q = value[1][n];
+    		q = value1[n];
     	else
-    		q = value[0][n];
+    		q = value2[n];
     	
     	if(count < 2 && q >= 0) {
     		return q;
@@ -52,9 +53,9 @@ public class maxSum{
     				q = r;
     		}
     		if(count > 0)
-    			q = value[1][n] = q + money[n];
+    			q = value1[n] = q + money[n];
     		else
-    			q = value[0][n] = q;
+    			q = value2[n] = q;
     	}
     	return q;
     }
