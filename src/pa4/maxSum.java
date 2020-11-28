58891b16bd9c1453b9525f3a3b571eefa6782b80
benel-del <52874329+benel-del@users.noreply.github.com>
2020-11-29 05:26:15 +0900

    [pa4] complete :100: 252us

--- src/pa4/maxSum.java
+++ src/pa4/maxSum.java
@@ -19,26 +19,25 @@ public class maxSum{
         int r = 0;
         int p = money.length;
         
-        value1 = new int[p];
-        value2 = new int[p];
+        value1 = new int[p];	// value1[i]: max when money[i] is used
+        value2 = new int[p];	// value2[i]: max when money[i] is not used
     	for(int i = 1; i < p; i++) {
     		value1[i] = value2[i] = -1;
     	}
-    	value2[0] = 0;
     	value1[0] = money[0];
 
-    	r = sum(p-1, 0);
-    	if(r < (p = sum(p-1, 1)))
+    	r = sum(p-1, 0);	// sum(p-1, 0): money[money.length-1] is not used
+    	if(r < (p = sum(p-1, 1)))	// sum(p-1, 1): money[money.length-1] is used
     		r = p;
     	
         return r;
     }
     
-    private int sum(int n, int count) {
+    private int sum(int n, int count) {	// n: array index, count: number of serial 1
     	int q;
-    	if(count > 0)
+    	if(count > 0)	// money[n] is used
     		q = value1[n];
-    	else
+    	else	// money[n] is not used
     		q = value2[n];
     	
     	if(count < 2 && q >= 0) {
@@ -47,7 +46,7 @@ public class maxSum{
     	
     	if(n > 0) {
     		q = sum(n-1, 0);
-    		if(count < 2) {
+    		if(count < 2) {	// possible for next index is used
     			int r;
     			if(q < (r = sum(n-1, count+1)))
     				q = r;
@@ -57,6 +56,6 @@ public class maxSum{
     		else
     			q = value2[n] = q;
     	}
-    	return q;
+    	return q;	// max when sum(n, count)
     }
 }
